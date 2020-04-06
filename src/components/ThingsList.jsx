7fd3cf6e05c783be6d8508d6f3c07a8f2246a499
nkra87 <nkwork874@gmail.com>
2020-04-07 00:03:40 +0900

    リスト保持、任意の値追加機能実装

--- src/components/ThingsList.jsx
+++ src/components/ThingsList.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
 import '../App.css';
 
-function ThingsList(props) {
-    const numbers = props.numbers;
-    const listThings = numbers.map(
-        (number) => <li>{number}</li>
-    );
-    return (
-        <ul>{listThings}</ul>
-    );
+class ThingsList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            thingsList: []
+        };
+    }
+
+    render () {
+        return (
+            <>
+            <form className="AddingForm" onSubmit={
+                event => {event.preventDefault();
+
+                const nameElement = event.currentTarget.elements["name"];
+                const locationElement = event.currentTarget.elements["location"];
+                this.setState(
+                    {
+                    thingsList: this.state.thingsList.concat({
+                        name: nameElement.value,
+                        location: locationElement.value
+                    })
+                    },
+                    // stateの変更後に入力した値を空にする
+                    () => {
+                    nameElement.value = "";
+                    locationElement.value = "";
+                    }
+                )
+                }
+            }>
+                <label>
+                    Name<input id="name" placeholder="追加したいものの名前" />
+                    Location<input id="location" placeholder="追加したいものを置く場所" />
+                </label>
+                <input type="submit" value="リストに追加"/>
+            </form>
+            <div className="ListDisp">
+                ここに配列の内容を表示<br />
+                <url>https://qiita.com/rioc/items/8723c236e10d989e827d#6-%E3%83%9C%E3%82%BF%E3%83%B3%E3%82%92%E6%8A%BC%E3%81%97%E3%81%A6todo%E3%82%92%E8%BF%BD%E5%8A%A0</url>
+            </div>
+            </>
+        )
+    }
 }
 
 export default ThingsList;
--- src/components/UserInterface.jsx
+++ src/components/UserInterface.jsx
@@ -8,10 +8,7 @@ function UserInterface() {
             <h1>Home Things Manager</h1>
             { /*リストの中身を動的に変更するような仕様にする*/ }
         </div>
-        <div className="MainField">
-            <button>リストに追加</button>
-            <button>リストを変更</button>
-        </div>
+        
         </>
     )
 }
