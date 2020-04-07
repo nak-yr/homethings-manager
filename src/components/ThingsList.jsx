1892616f27444d24dffc75f6aec745750646e92c
nkra87 <nkwork874@gmail.com>
2020-04-07 23:10:16 +0900

    保持しているリスト内容の出力機能実装、「memo」要素追加

--- src/components/ThingsList.jsx
+++ src/components/ThingsList.jsx
@@ -17,17 +17,20 @@ class ThingsList extends React.Component {
 
                 const nameElement = event.currentTarget.elements["name"];
                 const locationElement = event.currentTarget.elements["location"];
+                const memoElement = event.currentTarget.elements["memo"]
                 this.setState(
                     {
                     thingsList: this.state.thingsList.concat({
                         name: nameElement.value,
-                        location: locationElement.value
+                        location: locationElement.value,
+                        memo: memoElement.value
                     })
                     },
                     // stateの変更後に入力した値を空にする
                     () => {
                     nameElement.value = "";
                     locationElement.value = "";
+                    memoElement.value = "";
                     }
                 )
                 }
@@ -35,12 +38,22 @@ class ThingsList extends React.Component {
                 <label>
                     Name<input id="name" placeholder="追加したいものの名前" />
                     Location<input id="location" placeholder="追加したいものを置く場所" />
+                    Memo<input id="memo" placeholder="備考" />
                 </label>
                 <input type="submit" value="リストに追加"/>
             </form>
             <div className="ListDisp">
-                ここに配列の内容を表示<br />
-                <url>https://qiita.com/rioc/items/8723c236e10d989e827d#6-%E3%83%9C%E3%82%BF%E3%83%B3%E3%82%92%E6%8A%BC%E3%81%97%E3%81%A6todo%E3%82%92%E8%BF%BD%E5%8A%A0</url>
+                <ul>
+                    {this.state.thingsList.map((thing, index) =>
+                        <li key={index}>{thing.name}
+                            <ul>
+                                <li>{thing.location}</li>
+                                <li>{thing.memo}</li>
+                            </ul>
+                        </li>
+                        )
+                    }
+                </ul>
             </div>
             </>
         )
